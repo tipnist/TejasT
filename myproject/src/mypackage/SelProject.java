c917ebb4309794046e521de6b02c19ef7a632afb
tipnist <tipnist@gmail.com>
2016-03-02 16:23:47 -0600

    Update SelProject.java
    editing third time

--- myproject/src/mypackage/SelProject.java
+++ myproject/src/mypackage/SelProject.java
@@ -5,7 +5,8 @@ public class SelProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("First sample");
-System.out.println("second edit sample");
+System.out.println("second edit sample
+System.out.println("third edit sample");
 
 	}
 
